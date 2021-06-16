48e0d3e91904a5e802527a15d6de6e8d90cc0d1c
Courtney Frey <cfrey@launchcode.org>
2021-06-16 11:30:44 -0500

    starter code created

---
+++ Assignment2-ScrabbleScorer-csharp/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment2_ScrabbleScorer_csharp
+{
+    class Program
+    {
+
+        public static Dictionary<int, string> oldPointStructure = new Dictionary<int, string>()
+        {
+            {1, "A, E, I, O, U, L, N, R, S, T"},
+            {2, "D, G"},
+            {3, "B, C, M, P" },
+            {4, "F, H, V, W, Y" },
+            {5, "K" },
+            {8, "J, X" },
+            {10, "Q, Z" }
+        };
+
+
+
+        //Code your InitialPrompt method here
+
+
+
+
+
+
+        //Code your Transform method here
+
+
+
+
+
+
+        //Code your Scoring Option methods here
+
+
+
+
+
+
+        //Code your ScoringAlgorithms method here
+
+
+
+
+
+
+        //Code your RunProgram method here
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            //Call your RunProgram method here
+
+
+
+
+
+
+        }
+    }
+}
